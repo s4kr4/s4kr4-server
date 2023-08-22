9bc85a6f0bf0614ea40a06c0c1e82f41119db35b
s4kr4 <s4kr4m4@gmail.com>
2023-08-23 00:37:17 +0900

    Add argument types

--- src/libs/notify.ts
+++ src/libs/notify.ts
@@ -6,7 +6,7 @@ const { MASTODON_API_URL, MASTODON_TOKEN } = process.env
  * Mastodonにポストする
  * @param {string} text - 投稿本文
  */
-const postToMastodon = async (text) => {
+const postToMastodon = async (text: string) => {
   try {
     const params = {
       access_token: MASTODON_TOKEN,
@@ -24,7 +24,7 @@ const postToMastodon = async (text) => {
  * Blueskyにポストする
  * @param {string} text - 投稿本文
  */
-const postToBluesky = async (text) => {
+const postToBluesky = async (text: string) => {
   console.log('postToBluesky')
 }
 
