6528024eaac1c383421daf58e17734a14907e562
s4kr4 <s4kr4m4@gmail.com>
2023-08-22 21:26:50 +0900

    Update endpoint /api/lives/data

--- src/routes/lives.ts
+++ src/routes/lives.ts
@@ -1,10 +1,27 @@
 import Router from '@koa/router'
+import Parser from 'rss-parser'
 
+const FEED_URL = `https://www.youtube.com/feeds/videos.xml?channel_id=${process.env.YOUTUBE_CHANNEL_ID}`
+
+const parser = new Parser()
 const lives = new Router()
 
-lives.get('/data', (ctx, next) => {
-  ctx.body = {
-    method: 'get /'
+lives.get('/data', async (ctx, next) => {
+
+  try {
+    const feed = await parser.parseURL(FEED_URL)
+    const entries = feed.items.map((entry) => {
+      const videoId = entry.id.split(':')[2]
+      entry.videoId = videoId
+
+      return entry
+    })
+
+    ctx.body = {
+      data: entries,
+    }
+  } catch (error) {
+    console.warn(error)
   }
 })
 
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import Koa from 'koa'
 import json from 'koa-json'
+import 'dotenv/config'
+import cors from '@koa/cors'
 import router from './routes'
 
 const app = new Koa()
@@ -7,6 +9,7 @@ const app = new Koa()
 app.use(json())
 
 app
+  .use(cors())
   .use(router.routes())
   .use(router.allowedMethods())
 
